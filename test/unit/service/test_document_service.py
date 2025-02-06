ffda8c2490509f7f64d62a297052c866e346839f
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-02-06 14:17:47 +0000

    test-docs: doc service unit tests

--- test/unit/service/test_document_service.py
+++ test/unit/service/test_document_service.py
@@ -1,7 +1,7 @@
-'''
+"""
 Unit Test Document Service
 Author: Tom Aston
-'''
+"""
 
 from datetime import datetime
 from unittest.mock import AsyncMock, patch
@@ -57,3 +57,56 @@ class TestDocumentService:
             mock_repo.assert_called_once_with(
                 document_body=test_client_request, db=mock_db_session
             )
+
+    @pytest.mark.asyncio
+    async def test_get_by_id(self, mock_db_session: AsyncMock):
+        """
+        Test get by id
+        """
+        document = Document(
+            id=1,
+            title="test",
+            content="test",
+            description="test",
+            created=datetime.strptime("2021-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        )
+
+        with patch.object(
+            DocumentRepository, "get_by_id", AsyncMock(return_value=document)
+        ) as mock_repo:
+            response = await document_service.get_by_id(1, mock_db_session)
+
+            assert response.id == document.id
+            assert response.title == document.title
+            assert response.created == document.created
+
+            # ensure mock was called
+            mock_repo.assert_called_once_with(1, mock_db_session)
+
+    @pytest.mark.asyncio
+    async def test_delete_document(self, mock_db_session: AsyncMock):
+        """
+        Test delete document
+        """
+        deleted_document = Document(
+            id=1,
+            title="test",
+            content="test",
+            description="test",
+            created=datetime.strptime("2021-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        )
+
+        with patch.object(
+            DocumentRepository,
+            "delete_document",
+            AsyncMock(return_value=deleted_document),
+        ) as mock_repo:
+            response = await document_service.delete_document(1, mock_db_session)
+
+            assert (
+                response
+                == f"Document [id: {deleted_document.id}, title: {deleted_document.title}] deleted successfully"
+            )
+
+            # ensure mock was called
+            mock_repo.assert_called_once_with(1, mock_db_session)
