9e2df752246ba82cbf68ff0f07ac53bc437afcec
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-02-06 15:17:20 +0000

    chore: ruff formatting

--- app/nlp/preprocess.py
+++ app/nlp/preprocess.py
@@ -1,21 +1,21 @@
-'''
+"""
 Text Preprocessor
 Author: Tom Aston
-'''
-#inbuild dependencies
-import string
+"""
+
 import re
+import string
 from functools import reduce
 
-#external dependencies
 import nltk
-from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-nltk.download('stopwords')
+nltk.download("stopwords")
+
 
 class TextPreprocessor:
-    '''
+    """
     class to preprocess text for NLP
 
     Preproccessing includes:
@@ -26,85 +26,79 @@ class TextPreprocessor:
         - stemming i.e. walking -> walk
         - lemmatization i.e. running -> run, ate -> eat
         - remove whitespace
-    '''
+    """
 
     def __init__(self):
-        '''
+        """
         constructor for TextPreprocessor
-        '''
+        """
         self.stemmer = PorterStemmer()
 
     def get_cleaned_text(self, text: str) -> str:
-        '''
+        """
         public facing method for cleaning text strings for NLP models
-        '''
+        """
         preprocess_steps = [
             self._lowercase_text,
             self._remove_punctuation,
             self._remove_digits,
             self._remove_stop_words,
             self._remove_whitespace,
-            self._lemmatize_text
+            self._lemmatize_text,
         ]
         return reduce(lambda t, step: step(t), preprocess_steps, text)
-        
 
     def _lowercase_text(self, text: str) -> str:
-        '''
+        """
         return text in all lower case
-        '''
+        """
         return text.lower()
 
-
     def _remove_punctuation(self, text: str) -> str:
-        '''
+        """
         remove punctuation from text
-        '''
+        """
         table = str.maketrans({key: None for key in string.punctuation})
         return text.translate(table)
-    
 
     def _remove_digits(self, text: str) -> str:
-        '''
+        """
         remove digits and words containing digits
-        '''
-        return re.sub(r'\w*\d\w*', '', text).strip()
+        """
+        return re.sub(r"\w*\d\w*", "", text).strip()
 
-        
     def _remove_stop_words(self, text: str) -> str:
-        '''
+        """
         remove stopwords from text inc. like 'the', 'and', 'or'
-        '''
-        stop_words = set(stopwords.words('english'))
-        return ' '.join([word for word in str(text).split() if word not in stop_words])
-    
-    
+        """
+        stop_words = set(stopwords.words("english"))
+        return " ".join([word for word in str(text).split() if word not in stop_words])
+
     def _remove_whitespace(self, text: str) -> str:
-        '''
+        """
         remove whitespace from text
-        '''
-        return ' '.join(text.split())
-    
+        """
+        return " ".join(text.split())
 
     def _lemmatize_text(self, text: str) -> str:
-        '''
+        """
         lemmatize text
         i.e. running -> run, ate -> eat
-        '''
-        return ' '.join([self.stemmer.stem(word) for word in text.split()])
+        """
+        return " ".join([self.stemmer.stem(word) for word in text.split()])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     text_preprocessor = TextPreprocessor()
-    test_string = 'Hello my name is John'
+    test_string = "Hello my name is John"
     print(text_preprocessor.get_cleaned_text(test_string))
-    test_string = 'hello, how are you?'
+    test_string = "hello, how are you?"
     print(text_preprocessor.get_cleaned_text(test_string))
-    test_string = 'hello i am 20 years old and my name is nine9'
+    test_string = "hello i am 20 years old and my name is nine9"
     print(text_preprocessor.get_cleaned_text(test_string))
-    test_string = 'i am at the meeting call'
+    test_string = "i am at the meeting call"
     print(text_preprocessor.get_cleaned_text(test_string))
-    test_string = '  hello  how  are  you  '
+    test_string = "  hello  how  are  you  "
     print(text_preprocessor.get_cleaned_text(test_string))
-    test_string = 'running eating'
+    test_string = "running eating"
     print(text_preprocessor.get_cleaned_text(test_string))
--- app/nlp/similarity_calculator.py
+++ app/nlp/similarity_calculator.py
@@ -1,57 +1,54 @@
-'''
+"""
 Similarity Calculator
 Author: Tom Aston
-'''
+"""
 
-#local dependencies
-from preprocess import TextPreprocessor
-
-#external dependencies
 import spacy
+from preprocess import TextPreprocessor
 
 
 class SimilarityCalculator:
-    '''
+    """
     class to calculate similarity between two text strings
-    '''
+    """
+
     def __init__(self, preprocessor: TextPreprocessor) -> None:
-        '''
+        """
         constructor for SimilarityCalculator
-        '''
+        """
         self.preprocessor = preprocessor
-        self.nlp = spacy.load('en_core_web_md')
+        self.nlp = spacy.load("en_core_web_md")
 
     def calculate_similarity(self, text1: str, text2: str) -> float:
-        '''
+        """
         calculate the similarity between two text strings
-        '''
+        """
         cleaned_text1 = self.preprocessor.get_cleaned_text(text1)
         cleaned_text2 = self.preprocessor.get_cleaned_text(text2)
 
         return self._calculate_cosine_similarity(cleaned_text1, cleaned_text2)
-    
+
     def _calculate_cosine_similarity(self, text1: str, text2: str) -> float:
-        '''
+        """
         calculate the cosine similarity between two text strings
-        '''
+        """
         vector1 = self.nlp(text1)
         vector2 = self.nlp(text2)
 
         return vector1.similarity(vector2)
-    
 
-if __name__ == '__main__':
-    text1 = 'I am a cat'
-    text2 = 'I am a dog'
+
+if __name__ == "__main__":
+    text1 = "I am a cat"
+    text2 = "I am a dog"
 
     preprocessor = TextPreprocessor()
     similarity_calculator = SimilarityCalculator(preprocessor)
-    
-    
+
     similarity1 = similarity_calculator.calculate_similarity(text1, text2)
-    
-    text3 = 'There was a fire in one of the electrical cupboards'
-    text4 = 'Electrical cabinet found smoking'
+
+    text3 = "There was a fire in one of the electrical cupboards"
+    text4 = "Electrical cabinet found smoking"
 
     similarity2 = similarity_calculator.calculate_similarity(text3, text4)
 
