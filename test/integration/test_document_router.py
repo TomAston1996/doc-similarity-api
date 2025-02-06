4964920fed86ef4801a93dd7d903eafcedb5c883
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-02-06 14:11:25 +0000

    test-docs: add endpoint integration tests

--- test/integration/test_document_router.py
+++ test/integration/test_document_router.py
@@ -76,3 +76,76 @@ class TestDocumentRouter:
         assert response_json["id"] == create_response_json["id"]
         assert response_json["title"] == create_response_json["title"]
         assert response_json["created"] is not None
+
+    @pytest.mark.asyncio
+    async def test_get_all_documents(self, client: AsyncClient):
+        """
+        Test getting all document records
+        """
+        response = await client.get(DOCUMENT_BASE_URL)
+
+        response_json = response.json()
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response_json) > 0
+
+    @pytest.mark.asyncio
+    async def test_update_document(self, client: AsyncClient):
+        """
+        Test updating a document record
+        """
+        unix_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        test_create_request = {
+            "title": f"title_{unix_timestamp}",
+            "content": f"content_{unix_timestamp}",
+            "description": f"description_{unix_timestamp}",
+        }
+
+        create_response = await client.post(DOCUMENT_BASE_URL, json=test_create_request)
+
+        create_response_json = create_response.json()
+
+        update_request = {"title": "test_update"}
+
+        update_response = await client.patch(
+            f"{DOCUMENT_BASE_URL}{create_response_json['id']}", json=update_request
+        )
+
+        update_response_json = update_response.json()
+
+        assert update_response.status_code == status.HTTP_200_OK
+        assert update_response_json["id"] == create_response_json["id"]
+        assert update_response_json["title"] == update_request["title"]
+        assert update_response_json["content"] == create_response_json["content"]
+        assert (
+            update_response_json["description"] == create_response_json["description"]
+        )
+
+    @pytest.mark.asyncio
+    async def test_delete_document(self, client: AsyncClient):
+        """
+        Test deleting a document record
+        """
+        unix_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        test_create_request = {
+            "title": f"title_{unix_timestamp}",
+            "content": f"content_{unix_timestamp}",
+            "description": f"description_{unix_timestamp}",
+        }
+
+        create_response = await client.post(DOCUMENT_BASE_URL, json=test_create_request)
+
+        create_response_json = create_response.json()
+
+        delete_response = await client.delete(
+            f"{DOCUMENT_BASE_URL}{create_response_json['id']}"
+        )
+
+        get_response = await client.get(
+            f"{DOCUMENT_BASE_URL}{create_response_json['id']}"
+        )
+
+        assert delete_response.status_code == status.HTTP_200_OK
+        assert get_response.status_code == status.HTTP_404_NOT_FOUND
